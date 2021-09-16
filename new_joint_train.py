7ca70803610495d5a332597bbac0e6ad2f5a1a93
nvip <nvip12041994@gmail.com>
2021-09-16 21:48:49 +0700

    test

--- new_joint_train.py
+++ new_joint_train.py
@@ -529,7 +529,7 @@ def cli_main(
                         '--lr', '0.0005', '--clip-norm', '0.0',   
                         '--label-smoothing', '0.1', '--seed', '2048',
                         '--max-tokens', '15000',
-                        '--max-epoch', '18',
+                        '--max-epoch', '19',
                         '--lr-scheduler', 'inverse_sqrt',
                         '--weight-decay', '0.0',
                         '--user-dir', './user_dir',   
