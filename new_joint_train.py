acaf8559b7b9b8709e6048e001203da5d0ff739d
nvip <nvip12041994@gmail.com>
2021-09-30 07:08:12 +0700

    test

--- new_joint_train.py
+++ new_joint_train.py
@@ -544,7 +544,7 @@ def cli_main(
                         '--optimizer', 'adam', '--adam-betas', '(0.9, 0.98)', 
                         '--lr', '0.0005', '--clip-norm', '0.0',   
                         '--label-smoothing', '0.1', '--seed', '2048',
-                        '--max-tokens', '500',
+                        '--max-tokens', '5000',
                         '--max-epoch', '36',
                         '--lr-scheduler', 'inverse_sqrt',
                         '--weight-decay', '0.0',
