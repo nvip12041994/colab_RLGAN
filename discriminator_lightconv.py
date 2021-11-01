dd972664c1cb22f7d6aa0ffc0397a4d8890190d2
nvip <nvip12041994@gmail.com>
2021-11-02 05:31:55 +0700

    modify generate.py

--- discriminator_lightconv.py
+++ discriminator_lightconv.py
@@ -20,3 +20,32 @@ from fairseq.modules import (
     MultiheadAttention,
     PositionalEmbedding,
 )
+
+def Embedding(num_embeddings, embedding_dim, padding_idx):
+    m = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+    nn.init.normal_(m.weight, mean=0, std=embedding_dim ** -0.5)
+    nn.init.constant_(m.weight[padding_idx], 0)
+    return m
+
+
+def Linear(in_features, out_features, bias=True):
+    m = nn.Linear(in_features, out_features, bias)
+    nn.init.xavier_uniform_(m.weight)
+    if bias:
+        nn.init.constant_(m.bias, 0.0)
+    return m
+
+class Discriminator_lightconv(nn.Module):
+    def __init__(self, cfg, src_dict, dst_dict, use_cuda = True):
+        super(Discriminator_lightconv, self).__init__()
+    
+
+    def build_embedding(dictionary, embed_dim, path=None):
+        num_embeddings = len(dictionary)
+        padding_idx = dictionary.pad()
+        emb = Embedding(num_embeddings, embed_dim, padding_idx)
+        # if provided, load from preloaded dictionaries
+        if path:
+            embed_dict = utils.parse_embedding(path)
+            utils.load_embedding(embed_dict, dictionary, emb)
+        return emb
