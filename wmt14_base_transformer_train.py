8e8e9108aeeb898eea8b5ef00ca4d1bca95d6d8d
nvip-brain <nvip12041994@gmail.com>
2022-02-15 23:24:31 +0700

    wmt14 training file to transformer base as in paper

--- wmt14_base_transformer_train.py
+++ wmt14_base_transformer_train.py
@@ -462,7 +462,8 @@ def cli_main(
 ) -> None:
     parser = options.get_training_parser()
     lightconv_option = ['data-bin/wmt14_en_de',
-                        '--arch', 'transformer_vaswani_wmt_en_de_big',
+                        '--arch', 'transformer',
+                        
                         
                         '--optimizer', 'adam', '--adam-betas', '(0.9, 0.98)', '--weight-decay', '0.0',
                         '--lr', '0.0005', '--clip-norm', '0', '--weight-decay', '0.0',
@@ -472,7 +473,7 @@ def cli_main(
                         '--criterion', 'lightconv_label_smoothed_cross_entropy', '--label-smoothing', '0.1',
                         #'--seed', '2048',
                         
-                        '--max-tokens', '5000',
+                        '--max-tokens', '10000',
                         '--max-epoch', '4000',
                         
                         
